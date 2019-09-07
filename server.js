55adb3d150bff9896db773905658325df9a4c7c1
Jon Fobert <jonrfobert@gmail.com>
2019-09-07 12:19:18 -0400

    add port for heroku

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ if(process.env.NODE_ENV === 'production') {
     })
 }
 
-//const port = process.env.PORT || 3000
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`server started on Port ${port}`))
