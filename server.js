5ce603f4caeb0ab1011e2ac0fba68a4b6e4f84b2
Jon Fobert <jonrfobert@gmail.com>
2019-09-07 11:44:08 -0400

    version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const path = require('path');
 
 const cards = require('./routes/api/cards')
 
@@ -18,6 +19,15 @@ mongoose.connect(db, {useNewUrlParser: true})
 
 //Use Routes
 app.use('/api/cards', cards)
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(client/build))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+    })
+}
 
 //const port = process.env.PORT || 3000
 const port = 5000
